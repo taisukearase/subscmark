1a8cdd6ef8cbc646af04866fe06c2a2f9f12a7a4
arase taisuke <arase@qnote.co.jp>
2020-02-09 15:37:20 +0900

    [add] 一覧画面を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import Link from '@material-ui/core/Link'
+import NavBar from './components/NavBar'
+import LinkCard from './components/LinkCard'
 
-const App: React.FC = () => {
+function Copyright() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href='https://material-ui.com/'>
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
 }
 
-export default App;
+export default function App() {
+  interface BookMark {
+    id: number
+    title: string
+    url: string
+  }
+
+  const data: BookMark[] = [
+    {
+      id: 1,
+      title: '無印良品',
+      url: 'https://www.muji.com/jp/ja/store'
+    },
+    {
+      id: 2,
+      title: 'ユニクロ',
+      url: 'https://www.uniqlo.com/jp/'
+    },
+    {
+      id: 3,
+      title: 'テストおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお',
+      url: 'https://www.amazon.co.jp/?&tag=hydraamazonav-22&ref=pd_sl_7ibq2d37on_e&adgrpid=56100363354&hvpone=&hvptwo=&hvadid=289260145877&hvpos=1t1&hvnetw=g&hvrand=5806174922413686552&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1009307&hvtargid=aud-759377471893:kwd-10573980&hydadcr=27922_11415158&gclid=Cj0KCQiAvc_xBRCYARIsAC5QT9luL1CJpQOVegh7JMuYNUzaiEUv970WqCpi8wayTwYPCslXv-ueHjIaAhKzEALw_wcB'
+    }
+  ]
+
+  const list = data.map(data => (
+    <Box mb={4} key={data.id}>
+      <LinkCard url={data.url} title={data.title} key={data.id} />
+    </Box>
+  ))
+
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Container maxWidth='sm'>
+        <Box my={4}>
+          {list}
+          <Copyright />
+        </Box>
+      </Container>
+    </React.Fragment>
+  )
+}
---
+++ src/components/LinkCard.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import EditIcon from '@material-ui/icons/Edit'
+
+const useStyles = makeStyles({
+  card: {
+    display: 'flex'
+  },
+  title: {
+    maxWidth: '90%'
+  },
+  editButton: {
+    width: '10%',
+    display: 'flex',
+    justifyContent: 'center'
+  }
+})
+
+type ContentProps = {
+  title: string
+  url: string
+}
+
+const LinkCard: React.FC<ContentProps> = props => {
+  const classes = useStyles()
+  const { url, title } = props
+  return (
+    <Card className={classes.card}>
+      <CardActionArea
+        className={classes.title}
+        onClick={() => {
+          window.open(url)
+        }}>
+        <CardContent>
+          <Typography gutterBottom variant='body2' component='h2' noWrap={true}>
+            {title}
+          </Typography>
+          <Typography variant='body2' color='textSecondary' component='p' noWrap={true}>
+            {url}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActionArea
+        className={classes.editButton}
+        onClick={() => {
+          console.log('fuga')
+        }}>
+        <EditIcon />
+      </CardActionArea>
+    </Card>
+  )
+}
+
+export default LinkCard
