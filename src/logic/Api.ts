e983c4c51cd285405d809620da660b7441ed261c
arase taisuke <arase@qnote.co.jp>
2020-03-12 14:05:27 +0900

    エンドポイントを変更

--- src/logic/Api.ts
+++ src/logic/Api.ts
@@ -8,23 +8,23 @@ const getResponseData = ({ data }: AxiosResponse<any>): any => data
 axios.defaults.withCredentials = true
 
 export const getBookmarks = (): any => {
-  return axios.get(`${endpoint}/bookmarks`).then(getResponseData)
+  return axios.get(`${endpoint}`).then(getResponseData)
 }
 
 export const postBookmarks = (params: FormData): any => {
-  return axios.post(`${endpoint}/bookmarks`, params).then(getResponseData)
+  return axios.post(`${endpoint}`, params).then(getResponseData)
 }
 
 export const putBookmarks = (params: FormData): any => {
-  return axios.put(`${endpoint}/bookmarks`, params).then(getResponseData)
+  return axios.put(`${endpoint}`, params).then(getResponseData)
 }
 
 export const putBookmarksReadTime = (params: { id: number; lastReadTime: string }): any => {
-  return axios.put(`${endpoint}/bookmarks`, params).then(getResponseData)
+  return axios.put(`${endpoint}`, params).then(getResponseData)
 }
 
 export const deleteBookmarks = (id: number): any => {
-  return axios.delete(`${endpoint}/bookmarks`, { data: { id } }).then(getResponseData)
+  return axios.delete(`${endpoint}`, { data: { id } }).then(getResponseData)
 }
 
 export default null
