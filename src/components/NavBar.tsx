d4c9e10733a1e05fe42f8ee27a76e7a9ec9683a3
poipoisaurus <poipoisaurus@gmail.com>
2020-08-20 13:36:51 +0900

    [modify] ヘッダのデザインを変更

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { makeStyles, Theme } from '@material-ui/core/styles'
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core'
 import { Menu as MenuIcon } from '@material-ui/icons'
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -8,7 +8,8 @@ const useStyles = makeStyles((theme: Theme) => ({
     flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    position: 'absolute',
+    right: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
@@ -26,15 +27,14 @@ const NavBar: React.FC = () => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title} align="center">
             <span style={{ cursor: 'pointer' }} onClick={onReload}>
               Subscmark
             </span>
           </Typography>
-          <Button color="inherit">Login</Button>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
